e27ffa28ac4d358ae0d67792ceaf2eeda05fff90
shua <86475694+shuu-wasseo@users.noreply.github.com>
2023-07-06 12:07:08 +0800

    removed openpanel from Story.js

--- src/tabs/story/Story.js
+++ src/tabs/story/Story.js
@@ -1,14 +1,6 @@
 import { story } from '../../extra/lines'
 import Accordion from '../misc/Accordion'
 
-function openPanel(num) {
-  const panel = document.getElementById(`ch${num}`)
-  if (panel.style.display === "block") {
-    panel.style.display = "none";
-  } else {
-    panel.style.display = "block";
-  }}
-
 function Story({ num }) {
   return <Accordion num={num} head={"chapter " + num} body={story[num]} />
 }
