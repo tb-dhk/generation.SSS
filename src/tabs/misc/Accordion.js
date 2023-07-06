065e9b4c2785ec91c381158d33c5269703ef9321
shua <86475694+shuu-wasseo@users.noreply.github.com>
2023-07-06 12:21:32 +0800

    remove color background from reactmarkdown in accordion

--- src/tabs/misc/Accordion.js
+++ src/tabs/misc/Accordion.js
@@ -14,7 +14,7 @@ function Accordion({ num, head, body }) {
     <div className="chapter">
       <button className={`accordion s${num + 1}`} onClick={() => {openPanel(num)}}>{head}</button>
       <div className="story panel" id={`ch${num}`}>
-        <ReactMarkdown className={`s${num + 1}`} children={body} id="accordion" />
+        <ReactMarkdown children={body} id="accordion" />
       </div>
     </div>
   )
