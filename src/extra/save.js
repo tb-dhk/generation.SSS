87149c342e32563dc57646bb9d4009218b9d19de
shua <86475694+shuu-wasseo@users.noreply.github.com>
2023-07-06 11:58:28 +0800

    add base64 conversion and export function

--- src/extra/save.js
+++ src/extra/save.js
@@ -1,6 +1,10 @@
 export const import(str) {
-  const object = JSON.parse(str)
+  const object = JSON.parse(atob(str))
   for (let key in object) {
     localStorage.setItem(key, JSON.stringify(object[key]))
   }
+}
+
+export const export(str) {
+  const object = btoa(JSON.stringify(localStorage))
 }
