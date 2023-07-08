e8cc081376609b298db17e06e5feda257cb27bd1
Your Name <you@example.com>
2023-07-08 17:27:49 +0800

    Updates

--- src/tabs/story/Story.js
+++
@@ -1,8 +0,0 @@
-import { story } from '../../extra/lines'
-import Accordion from '../misc/Accordion'
-
-function Story({ num }) {
-  return <Accordion num={num} head={"chapter " + num} body={story[num]} />
-}
-
-export default Story 
