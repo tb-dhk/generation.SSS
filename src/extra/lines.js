5e2ef8649ea556a19a11d6f05e0c9d85d9294d6b
shuu-wasseo <joshuasoh2008@gmail.com>
2023-07-14 10:10:12 +0800

    rewritten story

--- src/extra/lines.js
+++ src/extra/lines.js
@@ -1,51 +1,61 @@
 export const story = [
   `
-you wake up in an empty room. there is no one else in the room, just you...and a big screen with words on it.
+you enter the mysterious room. no one else is there, just you...and a big blank screen.
 
-slowly, you get up and approach the screen as it flickers to life.
+you had been given instructions to go into the room on your first day at the new company, MODHAUS.
 
-> yoon seoyeon, you have chosen to become the first big S, or S1. out of many small s in the world, you are the first girl we discovered with the special ability to make S, a special kind of energy with supernatural properties.
-> 
-> you have been equipped with 2 S. these 2 S are very valuable and rare, but they will no longer be rare when we start mass producing them.
-> 
-> your job now is to generate as much S as possible. click the 'S1' button to spend 2 S to buy your first generator.
+"what is this place..." you wonder.
 
-and with that, you start your "adventure" as yoon seoyeon, stuck in a small room producing S until you can get the fuck out.
-    `,
+just a week ago, this mysterious company had offered you a job. "we have found recently that you have amazing potential," the letter they sent you wrote, "you have special abilities that no one else has, and that you do not know you possess."
+
+you had been studying to retake the suneung. but of course, tired of your mundane school life, you decided to leave and join this shady company instead.
+
+"i mean. what's the worst that could happen?"
+
+suddenly, the screen lights up.
+
+*yoon seoyeon, you have chosen to become the first big S, or S1. out of many small s in the world, you are the first girl we discovered with the special ability to make S, a special kind of energy with supernatural properties.* 
+
+*you have been equipped with 2 S. these 2 S are very valuable and rare, but they will no longer be rare when we start mass producing them.*
+
+*your job now is to generate as much S as possible. click the 'S1' button to spend 2 S to buy your first generator.*
+  `,
   `
-apprehensively, you press the button. suddenly, a machine appears in front of you.
+apprehensively, you press the little button labeled "S1". suddenly, a machine appears in front of you.
 
-> each S1 generator allows you to produce 0.001 S per millisecond. this number may increase over time, but what's important now is that you get as many generators as possible to produce S.
-> 
-> when you have 4 S, you will be able to introduce another big S into the room. the second big S (S2) will not generate S, but is equipped with a generator that generates your generators.
-> 
-> each subsequent big S generates generators of the previous big S. for example, S3 generators will generate S2 generators, which in turn generate S1 generators, which generate S. this allows your S production to increase exponentially.
-> 
-> additionally, for every S you buy, you increase the production by 1/24 (exponentially).
-> 
-> you must unlock each S in order. for example, S3 cannot be bought unless you have at least one S2 generator.
-> 
-> keep generating stars until you are able to unlock more members.
-    `,
+*each S1 generator allows you to produce 0.001 S per millisecond. this number may increase over time, but what's important now is that you get as many generators as possible to produce S.*
+
+*when you have 4 S, you will be able to introduce another big S into the room. the second big S (S2) will not generate S, but is equipped with a generator that generates your generators.*
+
+*each subsequent big S generates generators of the previous big S. for example, S3 generators will generate S2 generators, which in turn generate S1 generators, which generate S. this allows your S production to increase exponentially.*
+
+*additionally, for every generator you buy, you increase the production by 1/24 (exponentially).*
+
+*you must unlock and buy each S in order. for example, S3 cannot be bought unless you have at least one S2 generator.*
+
+*keep generating stars until you are able to unlock more members.*
+  `,
   `
-you press the "S2" button. suddenly, another girl appears next to you. the screen then lights up again.
+you press the "S2" button. suddenly, another girl comes in and sits down next to you. the screen then lights up again.
 
-> jeong hyerin, you have been chosen to become the second big S, or S2. out of many small s in the world, you are the second girl we discovered with the special ability to make S.
-> 
-> your job now is to generate as many S1 generators as possible. with yoon seoyeon, or S1, you will soon unlock 6 other S.
-> 
-> i will see you then.
+*jeong hyerin, you have been chosen to become the second big S, or S2. out of many small s in the world, you are the second girl we discovered with the special ability to make S.*
 
-"b-but," you stutter, "who are you? and why are we here? how did you find us?"
+*your job now is to generate as many S1 generators as possible. with yoon seoyeon, or S1, you will soon unlock 6 other S.*
 
-> my name is SUAH. we at MODHAUS searched far and wide through years and years of extensive research to find the 24 special girls who have the special ability.
-> 
-> your purpose will soon be made clear.
+*i will see you then.*
+
+"b-but," she stutters, "who are you? and why are we here? how did you find us?"
+
+*my name is SUAH. we at MODHAUS searched far and wide through years and years of extensive research to find 24 special girls who have the special ability. you two are just the first two.*
+
+*your purpose will soon be made clear.*
 
 and with that, the screen goes blank again.
-    `,
+  `,
   `
-after a few minutes of grinding, you finally have enough S to introduce S8 into the room, which the 7 of you have affectionately nicknamed the HAUS, despite it being a "room" and not a "house". 
+after minutes... or was it hours? you have no idea. you've lost track of time at this point.
+
+anyway, after a bunch of grinding, you finally have nearly enough S to introduce S8 into the room, which the 7 of you have affectionately nicknamed the HAUS, despite it being a "room" and not a "house". 
 
 at this point, five other members have been revealed. 
 S3 lee jiwoo,
@@ -54,93 +64,107 @@ S5 kim yooyeon,
 S6 kim soomin,
 and S7 kim nakyoung.
 
-the 7 of you have been hard at work, and have produced S in a rather short amount of time.
+the 7 of you have been hard at work, and have produced a lot of S in a rather short amount of time.
 
-"here goes nothing!" you quickly press the "S8" button, and the eighth girl walks in.
+"what's... e19?" soomin inquires.
+
+"oh its just ten quintillion," yooyeon quickly answers, "nothing much."
+
+"look!" hyerin points excitedly to the screen, "we have enough to bring in S8!"
+
+"here goes nothing!" you quickly press the "S8" button, and the eighth girl finally walks in.
 
 "hi...uh..." S8 walks in and waves shyly. "my name's gong yubin. do you guys know why we're here?"
 
-"don't worry too much about that yet. SUAH will be here any minute..." hyerin mutters.
+"don't worry too much about that yet. SUAH will be coming in any minute..." hyerin mutters.
 
 "SUAH? who's SUAH?" jiwoo asks.
 
 "you'll see." you sit the last girl down in front of the screen as it lights up once again.
 
-> greetings, seoyeon and hyerin. i see you have gathered the next 6 girls. good job.
-> 
-> to the other 6 of you, welcome. my name is SUAH, and i am an AI representative of MODHAUS, the secret organisation that has brought you all here.
-> 
-> as i told seoyeon and hyerin earlier, we at MODHAUS searched far and wide through years and years of extensive research to find the 24 special girls who have the special ability to produce S.
-> 
-> at the moment, we have only found the 8 of you, although many more are on the way.
-> 
-> right now, your task is to get to 1.333e33 S. such huge amounts of S are at the very limit of our capacity, so the S will combust and cause what is called a grand gravity, your first prestige layer. 
-> 
-> i will see you then.
+*greetings, seoyeon and hyerin. i see you have gathered the next 6 girls. good job.*
 
-"'prestige layer'...? this sounds like an idle game," hyerin remarked.
+*to the other 6 of you, welcome. my name is SUAH, and i am an AI representative of MODHAUS, the secret organisation that has brought you all here.*
 
-"that's because it is, you idiot." nakyoung deadpanned.
+*as i told seoyeon and hyerin earlier, we at MODHAUS searched far and wide through years and years of extensive research to find the 24 special girls who have the special ability to produce S.*
 
-"is no one going to talk about how SUAH said that the S will combust?" soomin's face was filled with worry.
+*at the moment, we have only found the 8 of you, although many more are on the way.*
 
-"we need to get out of here ASAP!" chaeyeon darted a few hurried glances around the room, looking for an exit.
+*right now, your task is to get to 1.333e33 S. such huge amounts of S are at the very limit of our capacity, so the S will combust and cause what is called a grand gravity, your first prestige layer. *
+
+*i will see you then.
+
+"'prestige layer'...? this sounds like an idle game," chaeyeon remarked.
+
+"that's because it is." nakyoung deadpanned.
+
+"is no one going to talk about how she said that the S will combust?" yubin's face was filled with worry.
+
+"we need to get out of here ASAP!" hyerin darted a few hurried glances around the room, looking for an exit.
 
 "do any of you have matches? we might be able to burn the place down." jiwoo shouted.
 
-"I HAVE A BOX!" yooyeon tossed her box of matches over, but yubin swiftly caught it.
+"I HAVE A BOX!" yooyeon tossed her box of matches over.
 
-"YOU'RE JUST GOING TO MAKE THE S COMBUST EVEN MORE! AND WE PROBABLY WON'T BE ABLE TO GET OUT OF HERE ANYWAY!" she screamed in jiwoo and yooyeon's faces.
+"I WANT TO GO HOME!" soomin screamed.
 
-"ooh, new girl's got an temper..." nakyoung subtly placed her hand over her mouth.
+"GUYS. CALM DOWN. WE'RE GOING TO LIVE. THEY PROBABLY WON'T KILL US HERE," seoyeon shouted to calm everyone down. 
 
-"ALRIGHT EVERYONE!" seoyeon clapped her hands to gather everyone's attention, exasperated. "we need to stay here and finish all of this so we can get out. we're going to have to trust SUAH and MODHAUS on this one. now everyone get back to work!"
+"are you sure?" soomin looked scared.
+
+"yes. yes. no one is dying here. not under my watch."
+
+everyone took in deep breaths to calm themselves down. jiwoo put her matches away.
+
+"ALRIGHT!" seoyeon clasped her hands together, struggling to keep her own cool. 
+
+"we need to stay here and finish all of this so we can get out. we're going to have to trust SUAH and MODHAUS on this one. now everyone get back to work!"
     `,
   `
 the smoke clears. the 8 of you emerge coughing, while the screen lights up again.
 
-> congratulations on your first grand gravity. you have now unlocked multiple different things to boost your S production.
+*congratulations on your first grand gravity. you have now unlocked multiple different things to boost your S production.
 
 chaeyeon grimaced. "there's more...?"
 
-> yes. after all, we still do not have all 24 girls yet.
-> 
-> anyway, back to the new things you've unlocked with the first grand gravity.
-> 
-> first thing you've unlocked is como. como will be used for another set of generators, and como progression will be much slower. these como generators will generate como power, which will boost your S production. you will also get (como power)^(1/24) como per second.
-> 
-> the second thing you've unlocked are objekts. the first 8 objekts unlock autobuyers for each of your generators so that your work will be less manual. the other 64 autobuyers boost the speed of the respective S's autobuyer.
-> 
-> the third thing you've unlocked is grand gravity challenges. finishing the first grand gravity was actually your first challenge, and you have completed it! the other 7 challenges boost your production, and to complete these, you will have to generate enough como for a grand gravity under harsh conditions. rewards for challenges also consist of one objekt and one como, of course.
-> 
-> your new task is to get all 72 objekts and finish all 8 challenges. i will see you again then. but before i leave...
+*yes. after all, we still do not have all 24 girls yet.*
+
+*anyway, back to the new things you've unlocked with the first grand gravity.*
+
+*first thing you've unlocked is como. como will be used for another set of generators, and como progression will be much slower. these como generators will generate como power, which will boost your S production. you will also get (como power)^(1/24) como per second.*
+
+*the second thing you've unlocked are objekts. the first 8 objekts unlock autobuyers for each of your generators so that your work will be less manual. the other 64 autobuyers boost the speed of the respective S's autobuyer.*
+
+*the third thing you've unlocked is grand gravity challenges. finishing the first grand gravity was actually your first challenge, and you have completed it! the other 7 challenges boost your production, and to complete these, you will have to generate enough como for a grand gravity under harsh conditions. rewards for challenges also consist of one objekt and one como, of course.*
+
+*your new task is to get all 72 objekts and finish all 8 challenges. i will see you again then. but before i leave...*
 
 a drawer popped out of the wall.
 
-> here is two como and one objekt. with every grand gravity, you will be given one set of these, but your como doubles every grand gravity.
+*here is two como and one objekt. with every grand gravity, you will be given one set of these, but your como doubles every grand gravity.*
 
 the objekt was a very smooth laminated piece of paper, and felt like a trading card but slightly thicker. the como, meanwhile, were shiny purple tokens.
 
 "interesting..." seoyeon mused. "so we need to do another...71 grand gravities before suah meets us again! EVERYBODY BACK TO WORK!"
-    `,
+  `,
   `
 you finally have 72 objekts. the big screen lights up again, and everyone turns to the bright light emitting from it.
 
-> *sighs* unfortunately, we have hit a wall.
+*sighs* unfortunately, we have hit a wall.
 
 "what?" yubin asked, "what do you mean, *a wall*?"
 
-> unfortunately, the ruler of this universe has not allowed us to proceed. this is the end of the road for now.
-> 
-> they will soon build the road ahead for you to proceed, but for now, all you can do is keep grinding for como.
-> 
-> however, to make your life a little bit easier, the owner has granted you a grand gravity autobuyer that you have unlocked at 72 objekts.
+*unfortunately, the ruler of this universe has not allowed us to proceed. this is the end of the road for now.*
+
+*they will soon build the road ahead for you to proceed, but for now, all you can do is keep grinding for como.*
+
+*however, to make your life a little bit easier, the owner has granted you a grand gravity autobuyer that you have unlocked at 72 objekts.
 
 "i dont like this guy," soomin pouted.
 
-"shush," jiwoo shushed soomin, "they might delete you."
+"shush," jiwoo shushed soomin, "they might fire you."
 
-> regardless of your opinions, i will see you soon.
+*regardless of your opinions, i will see you soon.
 
 and with that, the screen went blank for what would be a much longer time.
 
